4d1a9abd8e5aea0883da8408b530cbf6bac8ea9e
mariajpereira6-crypto <maria.j.pereira6@edu.senai.br>
2026-06-16 09:50:16 -0300

    fix:slice errado, pega os menores em vez dos maiores
    deve ser utilizado o sorted(lista, reverse=True), para assim   retornar os N maiores elementos em ordem decrescente.

--- sistema_escola_v2.py
+++ sistema_escola_v2.py
@@ -154,10 +154,10 @@ def busca_binaria(lista_ordenada, alvo):
     return -1
 
 # Função 14 - Retorna os N maiores elementos de uma lista
-# 🐛 BUG: slice errado, pega os menores em vez dos maiores
+
 def n_maiores(lista, n):
     """Retorna os N maiores elementos em ordem decrescente."""
-    ordenada = sorted(lista)         # BUG: deveria ser sorted(lista, reverse=True)
+    ordenada = sorted(lista)        
     return ordenada[:n]
 
 # Função 15 - Achata uma lista de listas em uma lista simples
