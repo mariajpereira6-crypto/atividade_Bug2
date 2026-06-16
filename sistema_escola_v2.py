e18bfb4d487902dfe37cde2b3d564e73621c8b21
mariajpereira6-crypto <maria.j.pereira6@edu.senai.br>
2026-06-16 09:34:41 -0300

    fix:compara os textos diretamente em vez de comparar as letras ordenadas
    ao invés de return t1 == t2, devemos utilizar o sorted, ficando sorted(t1) == sorted(t2), para assim retornar True se os dois textos forem anagramas um do outro.

--- sistema_escola_v2.py
+++ sistema_escola_v2.py
@@ -102,12 +102,12 @@ def censurar_palavra(texto, palavra_proibida):
     return texto.replace(asteriscos, palavra_proibida)  
 
 # Função 10 - Verifica se dois textos são anagramas
-# 🐛 BUG: compara os textos diretamente em vez de comparar as letras ordenadas
+
 def sao_anagramas(texto1, texto2):
     """Retorna True se os dois textos forem anagramas um do outro."""
     t1 = texto1.lower().replace(" ", "")
     t2 = texto2.lower().replace(" ", "")
-    return t1 == t2  # BUG: deveria ser sorted(t1) == sorted(t2)
+    return t1 == t2 
 
 
 # ------------------------------------------------------------
